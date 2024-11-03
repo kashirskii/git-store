45759f0db1ac22706498c7d511849628d9684915
kashirskii <kashirskiy.yegor@ya.ru>
2024-11-03 15:36:38 +0400

    updt: addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,12 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-
+import type { StorybookConfig } from '@storybook/nextjs';
+ 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
-  ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
-  },
+  framework: '@storybook/nextjs',
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
+  docs: {
+    autodocs: 'tag',
+  }
 };
+ 
 export default config;
